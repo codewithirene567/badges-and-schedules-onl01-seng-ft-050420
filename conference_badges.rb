7b4b8c92c257fff89acd59ad866fa5afc4fc010c
Irene Scott <irenejinscott@gmail.com>
2020-05-07 01:13:35 +0000

    Automatically backed up by Learn

--- conference_badges.rb
+++ conference_badges.rb
@@ -1,1 +1,27 @@
-# Write your code here.
+def badge_maker(name)
+  "Hello, my name is #{name}."
+end
+
+def batch_badge_creator(attendees)
+attendees.collect do |name|
+ "Hello, my name is #{name}."
+end
+end
+
+def assign_rooms(attendees)
+  room=0
+attendees.collect do |name|
+ room+=1
+ "Hello, #{name}! You'll be assigned to room #{room}!"
+end
+end
+
+def printer(attendees)
+  attendees = batch_badge_creator(attendees)
+  attendees = assign_rooms(attendees)
+    puts ""
+    
+   
+    
+  
+  
